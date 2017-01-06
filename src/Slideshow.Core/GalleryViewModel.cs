c961ae0d3cbb632ae19860b0205fc96518330642
Kamil Pajdzik <pajdziupaj@gmail.comn>
2017-01-05 21:55:40 -0800

    Refactor app to MVVM pattern
    Change the app name

---
+++ src/Slideshow.Core/GalleryViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using PropertyChanged;
+
+namespace Slideshow.Core
+{
+    [ImplementPropertyChanged]
+    public class GalleryViewModel
+    {
+        private readonly CoreDispatcher dispatcher;
+        private readonly PhotoLibrary photoLibrary;
+        private readonly Random random = new Random();
+
+        public GalleryViewModel(CoreDispatcher dispatcher)
+        {
+            this.photoLibrary = new PhotoLibrary();
+            this.dispatcher = dispatcher;
+        }
+
+        public ImageSource ImageSource { get; set; }
+
+        public bool IsLoading { get; set; }
+
+        public async Task Start()
+        {
+            this.IsLoading = true;
+            await this.LoadImages();
+            this.IsLoading = false;
+
+            await this.ShowPhoto();
+
+            ThreadPoolTimer.CreatePeriodicTimer(
+                async (source) =>
+                {
+                    await this.dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () => { await this.ShowPhoto(); });
+                }, TimeSpan.FromSeconds(10));
+        }
+
+        private async Task ShowPhoto()
+        {
+            var bitmap = await this.GetBitmapImage();
+            this.ImageSource = bitmap;
+        }
+
+        private async Task LoadImages()
+        {
+            await this.photoLibrary.Load();
+        }
+
+        private async Task<BitmapImage> GetBitmapImage()
+        {
+            var photo = this.PickNextPhoto();
+            var bitmap = await LoadImage(photo);
+
+            return bitmap;
+        }
+
+        private static async Task<BitmapImage> LoadImage(StorageFile file)
+        {
+            var bitmapImage = new BitmapImage();
+            var stream = (FileRandomAccessStream) await file.OpenAsync(FileAccessMode.Read);
+
+            await bitmapImage.SetSourceAsync(stream);
+
+            return bitmapImage;
+        }
+
+        private StorageFile PickNextPhoto()
+        {
+            var pickFromNewPhotos = this.random.Next()%2 == 1;
+            if (pickFromNewPhotos)
+            {
+                return this.GetRandomPhoto(this.photoLibrary.NewPhotos);
+            }
+
+            return this.GetRandomPhoto(this.photoLibrary.Photos);
+        }
+
+        private StorageFile GetRandomPhoto(IReadOnlyList<StorageFile> photos)
+        {
+            return photos[this.random.Next()%photos.Count];
+        }
+    }
+}
---
+++ src/Slideshow.Core/PhotoLibrary.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.System.Threading;
+
+namespace Slideshow.Core
+{
+    internal class PhotoLibrary
+    {
+        private const int NewPhotosLimit = 50;
+
+        private List<StorageFile> newPhotos;
+        private List<StorageFile> allPhotos;
+
+        public IReadOnlyList<StorageFile> Photos => new ReadOnlyCollection<StorageFile>(this.allPhotos);
+
+        public IReadOnlyList<StorageFile> NewPhotos => new ReadOnlyCollection<StorageFile>(this.newPhotos);
+
+        public async Task Load()
+        {
+            await this.LoadPhotos();
+
+            ThreadPoolTimer.CreatePeriodicTimer(async (source) =>
+            {
+                await this.LoadPhotos();
+            }, TimeSpan.FromMinutes(10));
+        }
+
+        private async Task LoadPhotos()
+        {
+            this.allPhotos = await this.LoadAllPhotos();
+            this.newPhotos = this.LoadNewPhotos(this.allPhotos);
+        }
+
+        private async Task<List<StorageFile>> LoadAllPhotos()
+        {
+            var files =
+                await this.GetPhotosFromFolderAndSubfolders(KnownFolders.PicturesLibrary);
+            return files;
+        }
+
+        private List<StorageFile> LoadNewPhotos(IEnumerable<StorageFile> photos)
+        {
+            var newPhotos = photos.OrderBy(photo => photo.DateCreated).Take(NewPhotosLimit);
+            return newPhotos.ToList();
+        }
+
+        private async Task<List<StorageFile>> GetPhotosFromFolderAndSubfolders(StorageFolder folder)
+        {
+            var files = new List<StorageFile>();
+
+            foreach (var subFolder in await folder.GetFoldersAsync())
+            {
+                var subFiles = await this.GetPhotosFromFolderAndSubfolders(subFolder);
+                files.AddRange(subFiles);
+            }
+
+            foreach (var file in await folder.GetFilesAsync())
+            {
+                if (IsProperImage(file))
+                {
+                    files.Add(file);
+                }
+            }
+
+            return files;
+        }
+
+        private static bool IsProperImage(StorageFile file)
+        {
+            return file.IsAvailable && file.ContentType.StartsWith("image");
+        }
+    }
+}
